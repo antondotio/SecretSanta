44fefe47eb210aad729b6f67406d88482153c331
Christina <christinalu42@gmail.com>
2019-09-15 19:12:47 -0600

    home

--- secretsantaapp/src/Home/home.jsx
+++ secretsantaapp/src/Home/home.jsx
@@ -5,22 +5,23 @@ import './home.css';
 const Home = () => {
   return (
     <div className="Home">
-      <header className="App-header">
-        <p>
-          Secret Santa
-        </p>
-        <p className="App-subheader">
-          Join group
-        </p>
-      </header>
-      <form>
-        <input type="text" placeholder="Group Code"></input><br></br>
-        <button type="submit">Submit</button><br></br> 
-        <button type="button" onClick={() => createGroup()}>Create group</button>
-      </form>
-
-      
-
+        
+        <header className="App-header">
+            <p>
+            Secret Santa
+            </p>
+            <a class="active" href="/home">Home</a>
+            <a href="/groups">Groups</a>
+            <a href="/wishlist">Wishlist</a><br></br>
+            <p className="App-subheader">
+            Join group
+            </p>
+        </header>
+        <form>
+            <input type="text" placeholder="Group Code"></input><br></br>
+            <button type="submit">Submit</button><br></br> 
+            <button type="button" onClick={() => createGroup()}>Create group</button>
+        </form>
     </div>
   );
 }
