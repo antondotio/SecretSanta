e6c0b22d0f14258bd80ec65ba55d1692972b8d6a
Anton Santos <43733827+antondotio@users.noreply.github.com>
2019-09-21 17:01:34 -0600

    Update home.jsx

--- secretsantaapp/src/Home/home.jsx
+++ secretsantaapp/src/Home/home.jsx
@@ -1,62 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './home.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-  Redirect
-} from "react-router-dom";
 
 
-class Home extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      groupCode: '',
-    };
-
-    this.handleGcodeChange = this.handleGcodeChange.bind(this);
-  }
-
-  handleGcodeChange(event){
-    this.setState({groupCode: event.target.value});
-  }
-
-  render(){
-    return (
-      <div className="Home">
-          
-          <header className="App-header">
-              <p>
-              Secret Santa
-              </p>
-              <a class="active" href="/home">Home</a>
-              <a href="/groups">Groups</a>
-              <a href="/wishlist">Wishlist</a><br></br>
-              <p className="App-subheader">
-              Join group
-              </p>
-          </header>
-          <form>
-            <input type="text" placeholder="Group Code" value={this.state.groupCode} onChange={this.handleGcodeChange}></input><br></br>
-            <Link to = {'/groups/:groupCode'} >
-              <button type="button" onClick={() => this.joinGroup()}>Join Group</button><br></br>
-            </Link> 
-            <Link to = {'/creategroup'} >
-              <button type="button" onClick={() => this.createGroup()}>Create Group</button><br></br>
-            </Link> 
-          </form>
-      </div>
-    );
-  }
-
-  joinGroup(){
-
-  }
-
-  createGroup(){
-
-  }
+const Home = () => {
+  return (
+    <div className="Home">
+        
+        <header className="App-header">
+            <p>
+            Secret Santa
+            </p>
+            <a class="active" href="/home">Home</a>
+            <a href="/groups">Groups</a>
+            <a href="/wishlist">Wishlist</a><br></br>
+            <p className="App-subheader">
+            Join group
+            </p>
+        </header>
+        <form>
+            <input type="text" placeholder="Group Code"></input><br></br>
+            <button type="submit">Submit</button><br></br> 
+            <button type="button" onClick={() => createGroup()}>Create group</button>
+        </form>
+    </div>
+  );
 }
+
+function createGroup(){
+
+}
+
 export default Home;
